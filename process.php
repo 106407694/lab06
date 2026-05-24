6045bfa527b27000490d398f72ec4badedcdbef8
Nahian Arshad <106407694@student.swin.edu.au>
2026-05-24 20:21:06 +0800

    Update process.php

--- process.php
+++ process.php
@@ -3,7 +3,7 @@ session_start();
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 $userToken = $_POST['userToken'] ?? '';
-if ($username === 'admin' && $password === '1234') {
+if ($username === 'Nahian' && $password === '106407694') {
     $_SESSION['user'] = $username;
     $_SESSION['token'] = $userToken;
     header("Location: welcome.php");
